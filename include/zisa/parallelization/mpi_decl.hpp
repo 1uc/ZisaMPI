cb86a01c620f2edabee5be16af4740d21c3d5ae5
Luc Grosheintz <luc.grosheintz@gmail.com>
2020-01-12 23:20:45 +0100

    Backup.

--- include/zisa/parallelization/mpi_decl.hpp
+++ include/zisa/parallelization/mpi_decl.hpp
@@ -35,12 +35,18 @@ Request isend(const array_const_view<T, n_dims, row_major> &arr,
               MPI_Comm comm);
 
 template <class T, int n_dims>
-Request irecv(array_view<T, n_dims, row_major> &arr,
+Request irecv(const array_view<T, n_dims, row_major> &arr,
               int sender,
               int tag,
               MPI_Comm comm);
 
 }
+
+template <class T, int n_dims>
+void bcast(const array_view<T, n_dims, row_major> &view,
+           int root,
+           const MPI_Comm &comm);
+
 }
 
 #endif // ZISA_MPI_DECL_HPP
--- include/zisa/parallelization/mpi_impl.hpp
+++ include/zisa/parallelization/mpi_impl.hpp
@@ -20,7 +20,25 @@ void send(const array_const_view<T, n_dims, row_major> &arr,
 }
 
 template <class T, int n_dims>
-Request irecv(array_view<T, n_dims, row_major> &arr,
+Request isend(const array_const_view<T, n_dims, row_major> &arr,
+              int receiver,
+              int tag,
+              MPI_Comm comm) {
+
+  auto ptr = (void *)arr.raw();
+  auto size = arr.size() * sizeof(T);
+  auto request = std::make_unique<MPI_Request>();
+
+  auto code
+      = MPI_Isend(ptr, size, MPI_BYTE, receiver, tag, comm, request.get());
+  LOG_ERR_IF(code != MPI_SUCCESS,
+             string_format("MPI_Isend failed. [%d]", code));
+
+  return Request(std::move(request));
+}
+
+template <class T, int n_dims>
+Request irecv(const array_view<T, n_dims, row_major> &arr,
               int sender,
               int tag,
               MPI_Comm comm) {
@@ -37,21 +55,15 @@ Request irecv(array_view<T, n_dims, row_major> &arr,
 }
 
 template <class T, int n_dims>
-Request isend(const array_const_view<T, n_dims, row_major> &arr,
-              int receiver,
-              int tag,
-              MPI_Comm comm) {
+void bcast(const array_view<T, n_dims, row_major> &view,
+           int root,
+           const MPI_Comm &comm) {
 
-  auto ptr = (void *)arr.raw();
-  auto size = arr.size() * sizeof(T);
-  auto request = std::make_unique<MPI_Request>();
+  auto ptr = (void *)view.raw();
+  auto size = view.size() * sizeof(T);
 
-  auto code
-      = MPI_Isend(ptr, size, MPI_BYTE, receiver, tag, comm, request.get());
-  LOG_ERR_IF(code != MPI_SUCCESS,
-             string_format("MPI_Isend failed. [%d]", code));
-
-  return Request(std::move(request));
+  auto code = MPI_Bcast(ptr, size, MPI_BYTE, root, comm);
+  LOG_ERR_IF(code != MPI_SUCCESS, string_format("MPI_Bcast failed. [%d]", code))
 }
 
 }
