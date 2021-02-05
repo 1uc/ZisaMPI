438c8153058b1a3591f0b87626b5164ecdb6df4a
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-02-05 15:18:43 +0100

    Improve dependency scripts.

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -1,10 +1,24 @@
 #! /usr/bin/env bash
 
-set -o xtrace
-
-if [[ "$#" -ne 2 ]]
+if [[ "$#" -lt 2 ]]
 then
-    echo "Usage: $0 COMPILER DESTINATION"
+    echo "Usage: $0 COMPILER DESTINATION [--zisa_has_mpi=ZISA_HAS_MPI]"
+fi
+
+for arg in "$@"
+do
+    case $arg in
+        --zisa_has_mpi=*)
+            ZISA_HAS_MPI=${arg#*=}
+            ;;
+        *)
+            ;;
+    esac
+done
+
+if [[ -z "${ZISA_HAS_MPI}" ]]
+then
+    ZISA_HAS_MPI=0
 fi
 
 component_name="ZisaMPI"
@@ -12,7 +26,7 @@ zisa_dependencies=("ZisaCore" "ZisaMemory")
 
 zisa_memory_root=$(realpath $(dirname $(readlink -f $0))/..)
 
-install_dir=$(${zisa_memory_root}/bin/install_dir.sh $1 $2)
+install_dir=$(${zisa_memory_root}/bin/install_dir.sh $1 $2 --zisa_has_mpi=${ZISA_HAS_MPI})
 source_dir=${install_dir}/sources
 
 echo ${install_dir}
@@ -29,14 +43,11 @@ fi
 mkdir -p ${source_dir}
 for dep in "${zisa_dependencies[@]}"
 do
-    echo ${src_dir}
     src_dir=${source_dir}/$dep
     git clone git@github.com:1uc/${dep}.git ${src_dir}
 
     mkdir -p ${src_dir}/build-dep
     cd ${src_dir}/build-dep
-
-    echo ${install_dir}
 
     cmake -DCMAKE_INSTALL_PREFIX=${install_dir}/zisa \
           -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa \
@@ -47,3 +58,6 @@ do
     cmake --build .
     cmake --install .
 done
+
+echo "The dependencies were installed at"
+echo "    ${install_dir}"
