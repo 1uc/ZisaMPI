0c26f52bbde4d46ba24d5c96e748eb7c8dd723b8
1uc <1uc@users.noreply.github.com>
2021-07-26 13:56:43 +0000

    deploy: 907d1173c93354eb32261fb3ae86ee4ec7c2889e

--- search/all_2.js
+++ search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['cmake_5',['CMake',['../md_cmake.html',1,'']]],
+  ['cmake_20usage_5',['CMake Usage',['../cmake_usage.html',1,'']]],
   ['cmake_2emd_6',['cmake.md',['../cmake_8md.html',1,'']]],
   ['comm_5fget_5fname_7',['comm_get_name',['../namespacezisa_1_1mpi.html#a6786cdfd48f1c964907437b11e638417',1,'zisa::mpi']]],
   ['comm_5fsplit_8',['comm_split',['../namespacezisa_1_1mpi.html#ad53acd1a3e1b5c37a384969968d71f05',1,'zisa::mpi']]],
--- search/pages_1.js
+++ search/pages_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['cmake_92',['CMake',['../md_cmake.html',1,'']]],
+  ['cmake_20usage_92',['CMake Usage',['../cmake_usage.html',1,'']]],
   ['conan_93',['Conan',['../md_conan.html',1,'']]],
   ['contributing_94',['Contributing',['../md_contributing.html',1,'']]]
 ];
